3313a6c99c8172b2b0c8e03f01fb9090685fdf0b
Anushka <anushkabahugunaa@gmail.com>
2021-06-14 09:43:16 +0530

    Updates

---
+++ precache-manifest.519680642f0b0d0404a036abb0d32409.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e31f1820e02cc74f2006",
+    "url": "/lights-out/static/css/main.19ee088c.chunk.css"
+  },
+  {
+    "revision": "e31f1820e02cc74f2006",
+    "url": "/lights-out/static/js/main.e31f1820.chunk.js"
+  },
+  {
+    "revision": "69765758761acc595027",
+    "url": "/lights-out/static/js/1.69765758.chunk.js"
+  },
+  {
+    "revision": "5cc5b55a4753d848057b",
+    "url": "/lights-out/static/js/runtime~main.5cc5b55a.js"
+  },
+  {
+    "revision": "96a107667b372804fc29bc612f538627",
+    "url": "/lights-out/static/media/Potra.96a10766.otf"
+  },
+  {
+    "revision": "6af8702451f5513abf12dccace7f5169",
+    "url": "/lights-out/index.html"
+  }
+];
